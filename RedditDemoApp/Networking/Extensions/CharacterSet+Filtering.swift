3fd7d498b4fe9e11ae5875c53a5297f7ac4c4dd1
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-06-10 00:40:09 -0500

    Adds helper methods

---
+++ RedditDemoApp/Networking/Extensions/CharacterSet+Filtering.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterSet.swift
+//  RedditDemoApp
+//
+//  Created by Alonso on 10/06/21.
+//
+
+import Foundation
+
+extension CharacterSet {
+
+    static let urlQueryValueAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@"
+        let subDelimitersToEncode = "!$&'()*+,;="
+
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+        return allowed
+    }()
+
+}
---
+++ RedditDemoApp/Networking/Extensions/Dictionary+Format.swift
@@ -0,0 +1,19 @@
+//
+//  Dictionary+Format.swift
+//  RedditDemoApp
+//
+//  Created by Alonso on 10/06/21.
+//
+
+extension Dictionary {
+
+    func percentEscaped() -> String {
+        return map { (key, value) in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            return escapedKey + "=" + escapedValue
+            }
+            .joined(separator: "&")
+    }
+
+}
